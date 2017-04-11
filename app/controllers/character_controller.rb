687bdf9f762ee0cf0d427f01afc63b452bf04bdb
Dylan Metas <dylanmetas@gmail.com>
2017-04-11 10:22:18 -0700

    fix character controller with do statment

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,25 +1,25 @@
-get '/users/:user_id/characters'
+get '/users/:user_id/characters' do
   erb :'characters/index'
 end
 
-get '/users/:user_id/characters/new'
+get '/users/:user_id/characters/new' do
   erb :'/characters/new'
 end
 
-post '/users/:user_id/characters'
+post '/users/:user_id/characters' do
   redirect '/'
 end
 
-get '/users/:user_id/characters/:id'
+get '/users/:user_id/characters/:id' do
   erb :'/characters/show'
 end
 
-get '/users/:user_id/characters/:id/edit'
+get '/users/:user_id/characters/:id/edit' do
   erb :'charactes/edit'
 end
 
-put '/users/:user_id/characters/:id'
+put '/users/:user_id/characters/:id' do
 end
 
-delete '/users/:user_id/characters/:id'
+delete '/users/:user_id/characters/:id' do
 end
